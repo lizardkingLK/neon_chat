34a505649c7b460878d8a3a25ce6789abc9afe5b
lizardkingLK . <chansanfdo@gmail.com>
2024-08-24 20:51:43 +0000

    added clerk
    added theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/navbar";
+import { ThemeProvider } from "@/components/themes/ThemeProvider";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={robotoMono.className}>{children}</body>
+      <body className={robotoMono.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="w-full">
+            {<Navbar />}
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
